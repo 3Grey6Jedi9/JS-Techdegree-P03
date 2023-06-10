5ad6d240b2000d22432edf075b23a876498ba7ad
Darth Vader <danieltarancon@gmail.com>
2023-06-10 13:09:11 +0200

    Updated

--- js/script.js
+++ js/script.js
@@ -3,6 +3,27 @@ document.addEventListener("DOMContentLoaded", function() {
 const nameInpupt = document.getElementById("name");
 nameInpupt.focus();
 
-
 })
 
+const other_job = document.getElementById("title");
+
+const other_job_role = document.getElementById("other-job-role");
+
+other_job_role.style.display = "none";
+
+other_job.addEventListener("change", function(){
+
+    const selectedOption = other_job.value;
+
+    switch (selectedOption) {
+
+        case "other":
+            other_job_role.style.display = "block";
+            break;
+        default:
+            other_job_role.style.display = "none";
+
+    }
+
+
+})
